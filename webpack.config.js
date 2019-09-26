fc1f3581e70e5d629ab417e0a0440cf3d76fe959
Vbobell <vinicius.bobel41@gmail.com>
2019-09-26 16:16:25 -0300

    Created structure webpack

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ASSET_PATH = process.env.ASSET_PATH || '/';
+
+module.exports = {
+    mode: 'development',
+    entry: path.join(__dirname, './index.js'),
+    output: {
+        publicPath: ASSET_PATH
+    },
+    devServer: {
+        historyApiFallback: true,
+        port: 3000,
+        watchOptions: {
+            aggregateTimeout: 300,
+            poll: 1000
+        }
+    },
+    module: {
+        rules: [{
+            test: /\.vue$/,
+            use: 'vue-loader'
+        }, {
+            test: /\.js$/,
+            use: 'babel-loader'
+        }, {
+            test: /\.(scss|css)$/,
+            use: [
+                'vue-style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        }]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+            inject: true
+        })
+    ]
+}
