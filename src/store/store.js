d2a06b8331d20d98ec6ef9dfcccadffc43f6884a
Vbobell <vinicius.bobel41@gmail.com>
2019-09-27 10:41:46 -0300

    Modify method buy product

--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,18 @@
-import Vuex from 'vuex'
-import Vue from 'vue'
+import Vuex from 'vuex';
+import Vue from 'vue';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
         products: [{
             'id': 1,
-            'name': 'TV'
+            'name': 'TV',
+            'amount': 4
         }, {
             'id': 2,
-            'name': 'Computer'
+            'name': 'Computer',
+            'amount': 2
         }]
     },
 
@@ -20,7 +22,21 @@ export default new Vuex.Store({
 
     mutations: {
         buyProduct(state, id) {
-            state.products = state.products.filter((product) => product.id != id);
+            let productNow;
+
+            state.products = state.products.map((product) => {
+                if (product.id == id) {
+                    product.amount--;
+                }
+
+                return product;
+            });
+            
+            productNow = state.products.filter((product) => product.id == id);
+
+            if (productNow.length > 0 && productNow[0].amount == 0) {
+                state.products = state.products.filter((product) => product.id != id);
+            }
         }
     },
 
